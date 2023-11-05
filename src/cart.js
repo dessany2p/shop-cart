2df73a6b7b308fbfab10679fce7dd6b3c66e0e47
Mr.Green <roopxh@gmail.com>
2023-11-05 20:14:04 +0600

    last changes

--- src/cart.js
+++ src/cart.js
@@ -1,36 +1,44 @@
 // window.localStorage.clear()
+// import countClicks from './script.js';
+
 const cartStorage = JSON.parse(localStorage.getItem('cart') || '[]')
 
 const newCard = document.querySelector('.add__cards')
 
+
+
 function createCart() {
    if (cartStorage.length) {
       cartStorage.forEach((element) => {
-         const { cartImg, cartTitle, cartPrice } = element
-         let item = newCard.cloneNode(true);
+         const { cartImg, cartTitle, cartPrice, cartCount, currentId } = element
+
+
 
          newCard.innerHTML =
             `
-         <div class="categories__item">
-            <img src=${cartImg} alt="" class="categories__image">
-            <span class="categories__name-item">${cartTitle}</span>
-            <div class="counter" data-counter>
-               <div class="counter__input">
-                  <input type="text" disabled value="1">
+            <div class="categories__item" id='${currentId}'>
+               <img src=${cartImg} alt="" class="categories__image">
+               <span class="categories__name-item">${cartTitle}</span>
+               <div class="counter" data-counter>
+                  <div class="counter__input">
+                     <input type="text" disabled value="${cartCount}">
+                  </div>
+                  <div class="counter__buttons-container">
+                     <button class="counter__button button__minus">⮟</button>
+                     <button class="counter__button button__plus">⮝</button>
+                  </div>
                </div>
-               <div class="counter__buttons-container">
-                  <button class="counter__button button__minus">⮟</button>
-                  <button class="counter__button button__plus">⮝</button>
+               <div class="categories__price">
+                  <span class="price__item">${cartPrice}</span>
                </div>
+               <span class="categories__close">X</span>
             </div>
-            <div class="categories__price">
-               <span class="price__item">${cartPrice}</span>
-            </div>
-            <span class="categories__close">X</span>
-         </div>
-         `
+            `
+         let clone = newCard.firstElementChild.cloneNode(true);
          console.log(newCard)
-         newCard.appendChild(item)
+         console.log(clone)
+         clone = newCard.cloneNode(true)
+
       });
    }
 }
@@ -42,19 +50,10 @@ const elems = document.querySelectorAll('.categories__close');
 function checkClose() {
    elems.forEach((elem, index) => {
       elem.addEventListener('click', () => {
-         const currentElem = elems[index]
-
-         let a = JSON.stringify(currentElem.parentElement.childNodes[1]);
+         let currentElem = elems[index]
+         console.log(currentElem)
+         let a = JSON.parse(currentElem);
          console.log(a)
-         if (localStorage.length) {
-
-            for (let item in localStorage.cart) {
-               let key = localStorage.key(item)
-               if (key === a) {
-                  localStorage.removeItem(key)
-               }
-            }
-         }
          currentElem.parentElement.remove();
 
       })
@@ -62,4 +61,6 @@ function checkClose() {
 }
 
 checkClose()
+
+// countClicks()
 
--- src/script.js
+++ src/script.js
@@ -7,52 +7,77 @@
 // console.log(mult(5, 4))
 
 const counters = document.querySelectorAll('[data-counter]');
+const cardsId = document.querySelectorAll('[data-id]');
 const priceButtons = document.querySelectorAll('.btn__buy');
 const cartItem = document.querySelectorAll('.card__item');
 const cart = document.querySelector("body > div.wrapper > div")
 
-priceButtons.forEach((el, i) => {
-   console.log(el);
 
-   // const btn = document.querySelectorAll('.btn__buy');
+let a = localStorage.getItem('cart')
+let btnCountTest = 0;
+
+priceButtons.forEach((el, i) => {
 
    el.addEventListener('click', (e) => {
-
       const cartTitle = el.parentElement.parentElement.childNodes[3].innerText
       const cartPrice = el.parentElement.parentElement.childNodes[5].childNodes[1].innerText
       const cartImg = el.parentElement.parentElement.querySelector('img').getAttribute('src')
+      let currentId = null
+
+      if (e && !el.classList.contains('added')) {
+         el.classList.add('added')
+         el.innerHTML = 'Добавлено в корзину'
+         btnCountTest++
+         // let kd = el.parentElement.document.querySelectorAll('[data-id]').dataset
+         currentId = el.parentNode.parentNode.dataset.id
+      }
+      else {
+         el.classList.remove('added')
+         el.innerHTML = 'Купить'
+         btnCountTest--
+         currentId = null
+      }
+      const cartCount = btnCountTest;
 
       const cartStorage = localStorage.getItem('cart') || '[]'
       const cart = JSON.parse(cartStorage)
-      const card = { cartImg, cartTitle, cartPrice }
+      const card = { cartImg, cartTitle, cartPrice, cartCount, currentId }
 
       localStorage.setItem('cart', JSON.stringify([...cart, card]))
-
    })
+
+
+
 })
 
-if (counters) {
-   counters.forEach(counter => {
-      counter.addEventListener('click', e => {
-         const target = e.target;
 
-         if (target.closest('.counter__button')) {
+function countClicks() {
+   if (counters) {
+      counters.forEach(counter => {
+         counter.addEventListener('click', e => {
+            const target = e.target;
 
-            let value = parseInt(target.closest('.counter').querySelector('input').value);
-            if (target.classList.contains('button__plus')) {
-               value++;
-            } else {
-               value--;
+            if (target.closest('.counter__button')) {
+
+               let value = parseInt(target.closest('.counter').querySelector('input').value);
+               if (target.classList.contains('button__plus')) {
+                  value++;
+               } else {
+                  value--;
+               }
+
+               if (value <= 0) {
+                  value = 0;
+                  target.closest('.counter').querySelector('.button__minus').classList.add('disabled')
+               } else {
+                  target.closest('.counter').querySelector('.button__minus').classList.remove('disabled')
+               }
+               target.closest('.counter').querySelector('input').value = value;
             }
-
-            if (value <= 0) {
-               value = 0;
-               target.closest('.counter').querySelector('.button__minus').classList.add('disabled')
-            } else {
-               target.closest('.counter').querySelector('.button__minus').classList.remove('disabled')
-            }
-            target.closest('.counter').querySelector('input').value = value;
-         }
+         })
       })
-   })
+   }
 }
+
+countClicks()
+
