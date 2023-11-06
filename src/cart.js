e9fdd7352776666eb28e6ee6b54f4950fb0c7d88
Mr.Green <roopxh@gmail.com>
2023-11-06 15:15:35 +0600

    last changes v2, working LS and TotalPrice. Need fix little troubles

--- src/cart.js
+++ src/cart.js
@@ -10,13 +10,11 @@ const newCard = document.querySelector('.add__cards')
 function createCart() {
    if (cartStorage.length) {
       cartStorage.forEach((element) => {
-         const { cartImg, cartTitle, cartPrice, cartCount, currentId } = element
+         const { currentId, cartTitle, cartPrice, cartCount, cartImg } = element
 
-
-
-         newCard.innerHTML =
-            `
-            <div class="categories__item" id='${currentId}'>
+         const div = document.createElement('categories__item')
+         div.innerHTML = `
+            <div class="categories__item" data-id='${currentId}'>
                <img src=${cartImg} alt="" class="categories__image">
                <span class="categories__name-item">${cartTitle}</span>
                <div class="counter" data-counter>
@@ -32,35 +30,119 @@ function createCart() {
                   <span class="price__item">${cartPrice}</span>
                </div>
                <span class="categories__close">X</span>
-            </div>
             `
-         let clone = newCard.firstElementChild.cloneNode(true);
-         console.log(newCard)
-         console.log(clone)
-         clone = newCard.cloneNode(true)
-
+         newCard.appendChild(div)
       });
    }
 }
+createCart();
 
-createCart()
+// function sumCarts() {
+//    let sum = 0;
+//    const div = document.querySelector(".sum");
+//    const totalPrice = document.querySelectorAll('.price__item')
+//    let value = Number(document.querySelector('input').value);
+//    console.log(value);
+//    console.log(sum)
+//    // console.log(totalPrice)
 
+//    for (let item of totalPrice) {
+//       // console.log(item)
+//       let a = item.outerText.slice(0, length - 5)
+
+//       // console.log(a)
+//       sum += parseFloat(a) * value
+//    }
+//    return div.innerHTML = `Итого: ${parseFloat(sum)} руб.`
+// }
+// sumCarts()
+
+function totalCount() {
+   let sum = 0;
+   const div = document.querySelector(".sum");
+   const totalPrice = document.querySelectorAll('.price__item')
+
+   for (let item of totalPrice) {
+      let value = Number(document.querySelector('input').value);
+      let a = Number(item.outerText.slice(0, length - 5));
+      // console.log(item, a)
+      sum += a * value
+   }
+   return div.innerHTML = `Итого: ${parseFloat(sum)} руб.`
+}
+totalCount()
+
+
+// countClicks()
+let counters = document.querySelectorAll('[data-counter]');
+function countClicks() {
+   if (counters) {
+      counters.forEach(counter => {
+         counter.addEventListener('click', e => {
+
+            const target = e.target;
+
+            if (target.closest('.counter__button')) {
+
+               let value = parseInt(target.closest('.counter').querySelector('input').value);
+               if (target.classList.contains('button__plus')) {
+                  for (let item of b) {
+                     if (item.currentId === target.parentNode.parentNode.parentNode.dataset.id) {
+                        item.cartCount += 1;
+                        localStorage.setItem('cart', JSON.stringify([...b]))
+                     }
+                  }
+                  value++;
+               } else {
+                  for (let item of b) {
+                     if (item.currentId === target.parentNode.parentNode.parentNode.dataset.id) {
+                        item.cartCount -= 1;
+                        localStorage.setItem('cart', JSON.stringify([...b]))
+                     }
+                  }
+                  value--;
+               }
+
+               if (value <= 0) {
+                  for (let item of b) {
+                     if (item.currentId === target.parentNode.parentNode.parentNode.dataset.id) {
+                        item.cartCount = 0;
+                        localStorage.setItem('cart', JSON.stringify([...b]))
+                     }
+
+                  }
+                  value = 0;
+                  target.closest('.counter').querySelector('.button__minus').classList.add('disabled')
+               } else {
+                  target.closest('.counter').querySelector('.button__minus').classList.remove('disabled')
+               }
+               target.closest('.counter').querySelector('input').value = value;
+            }
+         })
+      })
+   } totalCount()
+}
+
+countClicks()
+
+// checkClose()
 const elems = document.querySelectorAll('.categories__close');
-
 function checkClose() {
    elems.forEach((elem, index) => {
-      elem.addEventListener('click', () => {
-         let currentElem = elems[index]
-         console.log(currentElem)
-         let a = JSON.parse(currentElem);
-         console.log(a)
-         currentElem.parentElement.remove();
-
+      elem.addEventListener('click', (e) => {
+         let currentElem = elems[index].parentNode.dataset.id
+         for (let item of b) {
+            if (item.currentId === currentElem) {
+               // console.log(item.currentId)
+               // console.log(currentElem)
+               b.splice(index, 1)
+               // console.log(elem.parentNode)
+               elem.parentNode.remove()
+            }
+         }
+         sumCarts()
+         localStorage.setItem('cart', JSON.stringify([...b]));
       })
    })
 }
-
-checkClose()
-
-// countClicks()
-
+checkClose();
--- src/script.js
+++ src/script.js
@@ -6,15 +6,56 @@
 // console.log(mult(2, 4))
 // console.log(mult(5, 4))
 
-const counters = document.querySelectorAll('[data-counter]');
+
 const cardsId = document.querySelectorAll('[data-id]');
 const priceButtons = document.querySelectorAll('.btn__buy');
 const cartItem = document.querySelectorAll('.card__item');
-const cart = document.querySelector("body > div.wrapper > div")
+const cart = document.querySelector("body > div.wrapper > div");
 
 
-let a = localStorage.getItem('cart')
+// class LocalStorageUtil {
+//    constructor() {
+//       this.keyName = 'products';
+//    }
+
+//    getProducts() {
+//       const productsLocalStorage = localStorage.getItem(this.keyName);
+//       if (productsLocalStorage !== null) {
+//          return JSON.parse(productsLocalStorage);
+//       }
+//       return [];
+//    }
+//    putProducts(id) {
+//       let products = this.getProducts();
+//       let pushProduct = false;
+//       const index = products.indexOf(id);
+
+
+//       if (index === -1) {
+//          products.push(id);
+//          pushProduct = true
+//       } else {
+//          products.splice(index, 1)
+//       }
+//       localStorage.setItem(this.keyName, JSON.stringify(products))
+
+//       return {
+//          pushProduct: pushProduct,
+//          products: products
+//       }
+//    }
+// }
+
+// const localStorageUtil = new LocalStorageUtil();
+
+// localStorageUtil.getProducts('el1')
+
+
+
+// let a = localStorage.getItem('cart')
 let btnCountTest = 0;
+let b = JSON.parse(localStorage.getItem('cart'));
+console.log(b)
 
 priceButtons.forEach((el, i) => {
 
@@ -22,62 +63,48 @@ priceButtons.forEach((el, i) => {
       const cartTitle = el.parentElement.parentElement.childNodes[3].innerText
       const cartPrice = el.parentElement.parentElement.childNodes[5].childNodes[1].innerText
       const cartImg = el.parentElement.parentElement.querySelector('img').getAttribute('src')
-      let currentId = null
-
-      if (e && !el.classList.contains('added')) {
-         el.classList.add('added')
-         el.innerHTML = 'Добавлено в корзину'
-         btnCountTest++
-         // let kd = el.parentElement.document.querySelectorAll('[data-id]').dataset
-         currentId = el.parentNode.parentNode.dataset.id
-      }
-      else {
-         el.classList.remove('added')
-         el.innerHTML = 'Купить'
-         btnCountTest--
-         currentId = null
-      }
-      const cartCount = btnCountTest;
 
       const cartStorage = localStorage.getItem('cart') || '[]'
       const cart = JSON.parse(cartStorage)
-      const card = { cartImg, cartTitle, cartPrice, cartCount, currentId }
 
-      localStorage.setItem('cart', JSON.stringify([...cart, card]))
+      console.log(b)
+      let cartCount = 1;
+      let currentId = el.parentNode.parentNode.dataset.id
+      const card = { currentId, cartTitle, cartPrice, cartCount, cartImg }
+
+
+      if (!el.classList.contains('added')) {
+         console.log('bbbb', b)
+         b.forEach((item, i) => {
+            if (item[i] !== el) {
+               localStorage.setItem('cart', JSON.stringify([...cart, card]))
+            }
+         })
+
+         console.log(JSON.parse(localStorage.getItem('cart')))
+         el.classList.add('added')
+         console.log(el)
+         el.innerHTML = 'Добавлено в корзину'
+
+
+
+
+
+         localStorage.setItem('cart', JSON.stringify([...cart, card]))
+         b = JSON.parse(localStorage.getItem('cart'))
+
+      }
+      else if (el.classList.contains('added')) {
+         el.classList.remove('added')
+         el.innerHTML = 'Купить'
+         cartCount = 0;
+         b.splice(i, 1)
+         localStorage.setItem('cart', JSON.stringify([...b]))
+      }
    })
-
-
-
 })
 
 
-function countClicks() {
-   if (counters) {
-      counters.forEach(counter => {
-         counter.addEventListener('click', e => {
-            const target = e.target;
 
-            if (target.closest('.counter__button')) {
 
-               let value = parseInt(target.closest('.counter').querySelector('input').value);
-               if (target.classList.contains('button__plus')) {
-                  value++;
-               } else {
-                  value--;
-               }
-
-               if (value <= 0) {
-                  value = 0;
-                  target.closest('.counter').querySelector('.button__minus').classList.add('disabled')
-               } else {
-                  target.closest('.counter').querySelector('.button__minus').classList.remove('disabled')
-               }
-               target.closest('.counter').querySelector('input').value = value;
-            }
-         })
-      })
-   }
-}
-
-countClicks()
 
